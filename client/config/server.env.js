82f1c2859aceb137c90fb0bb7f07ed2d0ea90786
WdBly <603927378@qq.com>
2018-11-27 18:32:36 +0800

    root api

--- client/config/server.env.js
+++ client/config/server.env.js
@@ -4,7 +4,7 @@ var API_ROOT = require("./localhost.json")
 module.exports = {
     "process.env": {
         NODE_ENV:JSON.stringify(process.env.NODE_ENV || "production"),
-        API_ROOT:JSON.stringify(API_ROOT.API_ROOT)
+        API_ROOT:JSON.stringify(API_ROOT.API_ROOT_SERVER_PRODUCT)
     }
 };
 
